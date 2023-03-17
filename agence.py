5f177488965b839dc2d4a46de917d665d2d80f41
lina14b <104279809+lina14b@users.noreply.github.com>
2023-03-17 14:24:51 +0100

    Version Crud + save in csv

--- agence.py
+++ agence.py
@@ -1,22 +1,65 @@
 from voitures import Voiture
+from datetime import datetime
+
+import csv
+import os
 
 class Agence:
     def __init__(self):
         self.my_listcars = []
-    def ajouter(self,v):
-        #v=Voiture()
-        #v.Saisir()
-        v1=v 
-        v1.Afficher()
-        self.afficher()
-        self.my_listcars.append(v1)
-        print(self.my_listcars[0].matricule)
+        #initialize le fichiers csv
+        file_path = 'voitures.csv'
+        if os.path.getsize(file_path) == 0:
+         with open("voitures.csv", "w", newline="") as file:
+             writer = csv.writer(file,delimiter=';',quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+             writer.writerow(["Matricule", "Marque", "Date de circulation", "Kilometrage", "Cylindre"])
+    
+    def load(self):#lire les données d'un csv
+        self.my_listcars = []
+        # read the Voiture objects from the CSV file
+        with open("voitures.csv", "r", newline="") as file:
+            reader = csv.reader(file,delimiter=';')
+            header = next(reader) # skip header row
+            voitures = []
+            for row in reader:
+                print(row)
+
+                mat,marque,DC,km,cy=row
+                v=Voiture(mat,marque,DC,km,cy)
+                self.my_listcars.append(v)
+
+    def ajouter(self,v):#ajouter a un csv
+        v1=v        
+        with open("voitures.csv", "a", newline="") as file:
+         writer = csv.writer(file,delimiter=';',quoting=csv.QUOTE_MINIMAL, lineterminator="\n")
+         writer.writerow([v1.matricule, v1.marque, v1.date_circulation, v1.kilometrage, v1.cylindre])
+        
+        self.load()
+        
         
         
 
     def supprimer(self,v):
-        self.my_listcars.remove(v)
- 
+        self.load()
+        if(self.rechercherparmat(v.matricule)):
+            for voiture in self.my_listcars:
+             v1=Voiture()
+             v1=voiture 
+             #print(voiture.matricule)          
+            if(voiture.matricule==v.matricule):
+             self.my_listcars.remove(voiture)
+
+             with open("voitures.csv", "w", newline="") as file:
+              writer = csv.writer(file,delimiter=';',quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+              writer.writerow(["Matricule", "Marque", "Date de circulation", "Kilometrage", "Cylindre"])
+             for car in self.my_listcars:
+              v1=Voiture()
+              v1=car
+              v1.Afficher()
+              with open("voitures.csv", "a", newline="") as file:
+                writer = csv.writer(file,delimiter=';',quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+                writer.writerow([v1.matricule, v1.marque, v1.date_circulation, v1.kilometrage, v1.cylindre])
+    
     def rechercherparmat(self,mat):
         test=False
         for v in self.my_listcars:
@@ -28,19 +71,45 @@ class Agence:
         return test  
         
     def afficher(self):
-        print("_____________________________")
-        print(self.my_listcars)
+        self.load()
         x=0
-        for v in self.my_listcars:
-            print("\n--",x+1,"----------------------")
-            self.my_listcars[x].Afficher()
-            x=x+1
-            
+        print(len(self.my_listcars))
+
         print("_____________________________")
+        
+        for v in self.my_listcars:
+            print("\n------------------------")
+            self.my_listcars[x].Afficher()
+            x+=1
+        print("_____________________________")
+            
+            
+        
 
 
     def trier_date(self):
-        self.my_listcars.sort(key=lambda x: x.date_circulation)
+        print("sorting")
+        sorted_voitures = sorted(self.my_listcars, key=lambda x: datetime.strptime(x.date_circulation, "%d/%m/%Y"))
+        self.my_listcars.clear()
+        print("endsorting")
+        for voiture in sorted_voitures:
+            v1=Voiture()
+            v1=voiture
+            v1.Afficher()      
+            self.my_listcars.append(v1)
+            print("sorting")
+        with open("voitures.csv", "w", newline="") as file:
+              writer = csv.writer(file,delimiter=';',quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+              writer.writerow(["Matricule", "Marque", "Date de circulation", "Kilometrage", "Cylindre"])
+        for car in self.my_listcars:
+              v1=Voiture()
+              v1=car
+              v1.Afficher()
+              with open("voitures.csv", "a", newline="") as file:
+                writer = csv.writer(file,delimiter=';',quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
+                writer.writerow([v1.matricule, v1.marque, v1.date_circulation, v1.kilometrage, v1.cylindre])
+
+
     def getvoitureplusrecente(self):
         v=self.my_listcars[-1]
         v.Afficher()
@@ -81,23 +150,27 @@ def search_voiture(self, marque, date_circulation, cylindre, kilometrage):
 
 
 if __name__ == '__main__':
-    a =Agence()
-    v=Voiture()
-    v.Saisir()
-    a.ajouter(v)
-    v1=Voiture()
-    v1.Saisir()
-    a.ajouter(v1)
-    v2=Voiture()
-    v2.Saisir()
-    a.ajouter(v2)
-    found=a.rechercherparmat("12")
-    print(found)
-    
+    a=Agence()
     a.afficher()
+   # v=Voiture()
+  #  v.Saisir()
+    #print("-------Ajouter test-----")
+    #a.ajouter(v)
+    #a.afficher()
+    #print("-------supprimer test-----")
+    #a.supprimer(v)
+    #a.afficher()
+    #print("-------found test-----")
+    #found=a.rechercherparmat("503")
+    #print(found)
+    #print("-------End of found test-----")
+    
+   # a.afficher()
+    print("-------sorting test-----")
     a.trier_date()
     a.afficher()
+    print("-------plus anc plus rec test-----")
     a.getvoitureplusancienne()
     a.getvoitureplusrecente()
-    a.search_voiture()
+    # a.search_voiture()
     
--- voitures.py
+++ voitures.py
@@ -2,35 +2,46 @@ from datetime import datetime
 
 class Voiture:
     
-    def __init__(self):
-        matricule=""
-        marque=""
-        date_circulation=""
-        kilometrage=0
-        cylindre=0
+    # def __init__(self):
+    #     matricule=""
+    #     marque=""
+    #     date_circulation=""
+    #     kilometrage=0
+    #     cylindre=0
         
-    #def __init__(self, matricule, marque,date_circulation,Kilometrage,cylindre):
-     #   self.matricule=matricule
-      #  self.marque=marque
-       # self.date_circulation=date_circulation
-        #self.kilometrage=Kilometrage
-        #self.cylindre=cylindre
+    # def __init__(self, matricule, marque,date_circulation,Kilometrage,cylindre):
+    #     self.matricule=matricule
+    #     self.marque=marque
+    #     self.date_circulation=date_circulation
+    #     self.kilometrage=Kilometrage
+    #     self.cylindre=cylindre
+    
+    def __init__(self, matricule="", marque="", date_circulation="", kilometrage=0, cylindre=0):
+        self.matricule = matricule
+        self.marque = marque
+        self.date_circulation = date_circulation
+        self.kilometrage = kilometrage
+        self.cylindre = cylindre
+    
     def Saisir(self):
         
         self.matricule=input("matriqule: ")
         self.marque=input("marque: ")
         self.date_circulation=input("date de circulation: ")
-        self.date_circulation=datetime.strptime(self.date_circulation,'%d/%m/%Y')      
+        datec=datetime.strptime(self.date_circulation,'%d/%m/%Y') 
+        self.date_circulation=datec.strftime('%d/%m/%Y')
+        print(self.date_circulation)     
         self.kilometrage=input("Kilometrage: ")
         self.cylindre=input("Cylindre: ")
 
 
 
     def Afficher(self):
-       # datetime_object = datetime.strptime(datetime_str, '%m/%d/%y %H:%M:%S')
-        print(" {0:15s} | {1:8s} | {2:15s} | {3:15s} | {4:15s} |".format(self.matricule,self.marque,datetime.strftime(self.date_circulation, '%m/%d/%Y'),self.kilometrage,self.cylindre))
-        #print(self.matricule[0:15],"|",self.marque[0:8],"|",datetime.strftime(self.date_circulation, '%m/%d/%y'))
-    
+        datetime_object = datetime.strptime(self.date_circulation, '%d/%m/%Y')
+        date_string = datetime_object.strftime('%d/%m/%Y')
+        print(" {0:15s} | {1:8s} | {2:15s} | {3:15s} | {4:15s} |".format(self.matricule,self.marque,date_string,self.kilometrage,self.cylindre))
+      # print(" {0:15s} | {1:8s} | {2:15s} | {3:15s} | {4:15s} |".format(self.matricule,self.marque,self.date_circulation,self.kilometrage,self.cylindre))
+
     
 
 if __name__ == '__main__':
