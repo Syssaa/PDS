3707041dfa620f3f2a45c7d325a5aa39871e5d2a
lina14b <104279809+lina14b@users.noreply.github.com>
2023-03-17 16:35:20 +0100

    Final commit version manar before merge

--- voitures.py
+++ voitures.py
@@ -1,21 +1,52 @@
 from datetime import datetime
-class Voiture :
-    def __init__(self,matricule='',marque='', date_circulation=datetime.now(),kilometrage='',cylindre=''):  
-       self.matricule = matricule
-       self.marque = marque
-       self. date_circulation=  date_circulation
-       self.kilometrage = kilometrage
-       self.cylindre = cylindre
-    def affiche(self):
-        print((" {0:<8} | {1:<9} | {2:<15s} |  {3:<8}| {4:<10} ").format(self.matricule,self.marque,self.date_circulation.strftime('%d-%m-%Y'),self.kilometrage,self.cylindre))
 
-def Saisir(self):self.matricule=input("matriqule: ")
-self.marque=input("marque:")
-self.date_circulation=input("date de circulation: ")
-self.date_circulation=datetime.strftime(self.date_circulation,'%d/%m/%Y')  
-self.kilometrage=input("Kilometrage: ")
-self.cylindre=input("Cylindre: ")
-if _name__=='__main_':
-    v=Voiture()
-    v.affiche()
-    v.Saisir()
+class Voiture:
+    
+    # def __init__(self):
+    #     matricule=""
+    #     marque=""
+    #     date_circulation=""
+    #     kilometrage=0
+    #     cylindre=0
+        
+    # def __init__(self, matricule, marque,date_circulation,Kilometrage,cylindre):
+    #     self.matricule=matricule
+    #     self.marque=marque
+    #     self.date_circulation=date_circulation
+    #     self.kilometrage=Kilometrage
+    #     self.cylindre=cylindre
+    
+    def __init__(self, matricule="", marque="", date_circulation="", kilometrage=0, cylindre=0):
+        self.matricule = matricule
+        self.marque = marque
+        self.date_circulation = date_circulation
+        self.kilometrage = kilometrage
+        self.cylindre = cylindre
+    
+    def Saisir(self):
+        
+        self.matricule=input("matriqule: ")
+        self.marque=input("marque: ")
+        self.date_circulation=input("date de circulation: ")
+        datec=datetime.strptime(self.date_circulation,'%d/%m/%Y') 
+        self.date_circulation=datec.strftime('%d/%m/%Y')
+        print(self.date_circulation)     
+        self.kilometrage=input("Kilometrage: ")
+        self.cylindre=input("Cylindre: ")
+
+
+
+    def Afficher(self):
+        datetime_object = datetime.strptime(self.date_circulation, '%d/%m/%Y')
+        date_string = datetime_object.strftime('%d/%m/%Y')
+        print(" {0:15s} | {1:8s} | {2:15s} | {3:15s} | {4:15s} |".format(self.matricule,self.marque,date_string,self.kilometrage,self.cylindre))
+      # print(" {0:15s} | {1:8s} | {2:15s} | {3:15s} | {4:15s} |".format(self.matricule,self.marque,self.date_circulation,self.kilometrage,self.cylindre))
+
+    
+
+if __name__ == '__main__':
+     print("hello")
+     
+     v=Voiture()
+     v.Saisir()
+     v.Afficher()
